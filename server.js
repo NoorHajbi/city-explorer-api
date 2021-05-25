95d03d0963e9b355ae138b8484e285a46b2f51eb
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-05-25 21:55:38 +0300

    try heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
-const express = require ('express');
+const express = require ('express'); //when you require it you need to install  (npm i express)
+require('dotenv').config(); // npm i dotenv
 const server = express();
 // const PORT = 3001;
 const PORT = process.env.PORT;
