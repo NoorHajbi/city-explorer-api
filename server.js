031de83206570237f4fdf9862d4acde65306e33a
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-05-25 21:48:55 +0300

    inital commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 'use strict';
 const express = require ('express');
 const server = express();
-const PORT = 3001;
+// const PORT = 3001;
+const PORT = process.env.PORT;
+
 server.listen (PORT,()=> {
     console.log (`listening on PORT ${PORT}`);
 })
