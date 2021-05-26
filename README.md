41942abbb79f2411a780d967b38f8187e8456228
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-05-26 03:36:06 +0300

    lab07

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # city-explorer-server
+
+# Project Name
+
+**Author**: Noor Hajbi
+**Version**: 
+## Overview
+<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+
+## Credit and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+
+
+Time Estimates
+For each of the lab features, make an estimate of the time it will take you to complete the feature, and record your start and finish times for that feature:
+
+Name of feature: Weather 
+
+Estimate of time needed to complete: 3 hours
+
+Start time: 3:30 pm
+
+Finish time: 3:30 am
+
+Actual time needed to complete: not sure, but longer than estimated
--- server.js
+++ server.js
@@ -1,11 +1,55 @@
 'use strict';
 //All requires should be on the top
-const express = require ('express'); //when you require it you need to install  (npm i express)
+const express = require('express'); //when you require it you need to install  (npm i express)
 require('dotenv').config(); // npm i dotenv
-const server = express();
-// const PORT = 3001;
-const PORT = process.env.PORT;
+const weatherData = require('./data/weather.json')
+const cors = require('cors'); //understand it more
 
-server.listen (PORT,()=> {
-    console.log (`listening on PORT ${PORT}`);
+const server = express();
+server.use(cors()); //  make my server opened for anyone
+
+const PORT = process.env.PORT; // <=> const PORT = 3001;
+
+
+// http://localhost:3001/
+server.get('/', (req, res) => {
+    res.send('home route')
+})
+
+// http://localhost:3001/weather?(values)
+class Forecast {
+    constructor(date, description) {
+        this.date = date;
+        this.description = description;
+    }
+}
+server.get('/weather', (req, res) => {
+    console.log(req.query)
+    let cityData = weatherData.results.find(item => {
+        if (item.city_name.toLowerCase() == req.query.searchQuery.toLowerCase()
+            && item.lat.toLowerCase() == req.query.lat.toLowerCase()
+            && item.lon.toLowerCase() == req.query.lon.toLowerCase())
+            return item
+    })
+    let forecastArr=[];
+    res.send(cityData)
+})
+
+// Using each data point from the static data of the city that the user searched, 
+// create an array of `Forecast` objects, one for each day.
+//  Do the necessary data wrangling to ensure the objects you create contain the information
+//   as required for correct client rendering. See the sample response.
+// Send the full array back to the client who requested data from the `weather` endpoint.
+server.get('*', (req, res) => {
+    if(res.status(400))
+    res.send('Bad Request');
+    else if(res.status(404))
+    res.send('Not Found');
+    else if(res.status(500))
+    res.send('Internal Server Error');
+})
+
+
+server.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`);
 })
