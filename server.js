ab774c2bb981c4821d394255c4da27c46a3cd12b
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-05-29 22:53:07 +0300

    lab08 weather

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const axios = require('axios');
 const PORT = process.env.PORT // <=> const PORT = 3001;
 const server = express();
 server.use(cors()); //  make my server opened for anyone
+const weatherKey = process.env.WEATHER_API_KEY;
 
 
 
@@ -36,7 +37,8 @@ class Forecast {
 
 // http://localhost:3001/weather?searchQuery=Amman
 
-server.get('/weather', (req, res) => {
+server.get('/weather',weatherHandler)
+ function weatherHandler (req, res) {
     // console.log(req.query)
     let cityData = weatherData.find(item => {
         if (req.query.searchQuery.toLowerCase() == item.city_name.toLowerCase()
@@ -46,27 +48,32 @@ server.get('/weather', (req, res) => {
             return item;
     })
     console.log(cityData);
-    try {
-        let forecastArr = []
-        forecastArr.push(cityData.data.map(item => {
-            return new Forecast(item);
-        }))
-        res.send(forecastArr);
-    }
-    catch (error) {
-        res.status(500).send(`error in getting the weather data ==> ${error}`);
-    }
-})
 
-server.get('*', (req, res) => { // * means all, so for errors we should put it in last
-    if (res.status(400))
-        res.send('Bad Request');
-    else if (res.status(404))
-        res.send('Not Found');
-})
+    const weatherbitURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${req.query.searchQuery}&key=${weatherKey}`;
+    axios
+        .get(weatherbitURL)
+        .then(result => {
+            let forecastArr = []
+            // forecastArr.push(cityData.data.map(item => {
+            forecastArr.push(result.data.data.map(item => {
+                return new Forecast(item);
+            }))
+            res.send(forecastArr);
+        })
+        .catch(error => {
+            res.status(500).send(`error in getting the weather data ==> ${error}`);
+        }
+        )
+    }   
+    server.get('*', (req, res) => { // * means all, so for errors we should put it in last
+        if (res.status(400))
+            res.send('Bad Request');
+        else if (res.status(404))
+            res.send('Not Found');
+    })
 
 
-server.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
-})
+    server.listen(PORT, () => {
+        console.log(`Listening on PORT ${PORT}`)
+    })
 
