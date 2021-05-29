db93cd7cf108c3c253b43479241f153d9f6aa1a1
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-05-29 23:15:25 +0300

    lab09 inital commit

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const axios = require('axios');
 const PORT = process.env.PORT // <=> const PORT = 3001;
 const server = express();
 server.use(cors()); //  make my server opened for anyone
-const weatherKey = process.env.WEATHER_API_KEY;
+const weatherHandler = require('./modules/weather.js');
+
 
 
 
@@ -22,12 +23,7 @@ server.get('/test', (req, res) => {
     res.send('test')
 })
 
-class Forecast {
-    constructor(item) {
-        this.date = item.valid_date; //As a JSON-File
-        this.description = item.weather.description;
-    }
-}
+
 
 // Using each data point from the static data of the city that the user searched, 
 // create an array of `Forecast` objects, one for each day.
@@ -38,33 +34,8 @@ class Forecast {
 // http://localhost:3001/weather?searchQuery=Amman
 
 server.get('/weather',weatherHandler)
- function weatherHandler (req, res) {
-    // console.log(req.query)
-    let cityData = weatherData.find(item => {
-        if (req.query.searchQuery.toLowerCase() == item.city_name.toLowerCase()
-            // && req.query.lat== item.lat 
-            // && req.query.lon ==  item.lon
-        )
-            return item;
-    })
-    console.log(cityData);
 
-    const weatherbitURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${req.query.searchQuery}&key=${weatherKey}`;
-    axios
-        .get(weatherbitURL)
-        .then(result => {
-            let forecastArr = []
-            // forecastArr.push(cityData.data.map(item => {
-            forecastArr.push(result.data.data.map(item => {
-                return new Forecast(item);
-            }))
-            res.send(forecastArr);
-        })
-        .catch(error => {
-            res.status(500).send(`error in getting the weather data ==> ${error}`);
-        }
-        )
-    }   
+      
     server.get('*', (req, res) => { // * means all, so for errors we should put it in last
         if (res.status(400))
             res.send('Bad Request');
